df5ce1094f57a5f6e549c5d0214913d8b5eaeda0
Jing Chen <jing.chen2@ibm.com>
2025-02-28 13:09:19 -0500

    Another try
    Signed-off-by: Jing Chen <jing.chen2@ibm.com>

--- tests/test_examples_run.py
+++ tests/test_examples_run.py
@@ -98,7 +98,6 @@ TO_SKIP = {
         / "rag_library1.pdl",  # (This is glue to Python, it doesn't "run" alone)
         pathlib.Path("pdl-live-react") / "demos" / "error.pdl",
         pathlib.Path("pdl-live-react") / "demos" / "demo1.pdl",
-
         # For now, skip the granite-io examples
         pathlib.Path("examples") / "granite-io" / "granite_io_hallucinations.pdl",
         pathlib.Path("examples") / "granite-io" / "granite_io_openai.pdl",
